30e5fa5fc3c42d7979e93a75f7fa3245eee260af
Munib Ahmed <munibahmed016@gmail.com>
2024-06-06 13:35:17 +0500

    footer

--- app/libs/Footer.jsx
+++ app/libs/Footer.jsx
@@ -7,12 +7,10 @@ import { FaYoutube } from "react-icons/fa";
 function Footer() {
 	return (
 		<>
-			<div className="bg-gray-50 h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
+			<div className="bg-gray-900 h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
 				<div className="p-5 ">
 					<ul>
-						<p className="text-gray-800 font-bold text-3xl pb-6">
-							Stream<span className="text-blue-600">line</span>
-						</p>
+						<img src="/3z_bio_logo.svg" alt="3Z-Bio" className="w-44 h-44" />
 						<div className="flex gap-6 pb-5">
 							<FaInstagram className="text-2xl cursor-pointer hover:text-yellow-600" />
 							<FaTwitter className="text-2xl cursor-pointer hover:text-blue-600" />
@@ -23,57 +21,57 @@ function Footer() {
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Product</p>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Stocks
+						<p className="text-white font-bold text-2xl pb-4">HELP TO HELP </p>
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Order Status 
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Futures & Options
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Shipping & Delivery 
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Mutual Funds
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Return Policy
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Fixed deposits
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Payments 
 						</li>
-					</ul>
-				</div>
-				<div className="p-5">
-					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Company</p>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							About
-						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Products
-						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Pricing
-						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Careers
-						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Press & Media
+            <li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Download App  
 						</li>
 					</ul>
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Support</p>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<p className="text-white font-bold text-2xl pb-4">STORE POLICES </p>
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Delivery Information 
+						</li>
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Refunds & Return 
+						</li>
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Careers At Lush 
+						</li>
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+							Our Policies 
+						</li>
+					</ul>
+				</div>
+				<div className="p-5">
+					<ul>
+						<p className="text-white font-bold text-2xl pb-4">Support</p>
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Contact
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Support Portals
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							List Of Charges
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Downloads & Resources
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-white text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Videos
 						</li>
 					</ul>
@@ -81,10 +79,7 @@ function Footer() {
 			</div>
 			<div className="flex flex-col justify-center items-center text-center  p-5 bg-gray-50">
 				<h1 className=" text-gray-800 font-semibold">
-					© 2021-2022 All rights reserved | Build with ❤ by{" "}
-					<span className="hover:text-blue-600 font-semibold cursor-pointer">
-						streamline{" "}
-					</span>
+					© 2021-2022 All rights reserved 
 				</h1>
 			</div>
 		</>
